d87f407cf740461d7bbc7661561154830940014f
mdsibtainhabib <sibtainhabib413@gmail.com>
2024-12-15 11:20:26 +0530

    connecting lines

--- src/components/Tournament/Tournament.tsx
+++ src/components/Tournament/Tournament.tsx
@@ -16,7 +16,7 @@ const Tournament = () => {
       image: "/tournament-img/table-tennis.png",
     },
     {
-      name: "CRIKET",
+      name: "CRICKET",
       image: "/tournament-img/Cricket.png",
     },
     {
@@ -46,13 +46,14 @@ const Tournament = () => {
   ];
 
   return (
-    <div className="w-full bg-black  flex flex-col items-center px-6 md:px-16 mb-6">
+    <div className="w-full bg-black flex flex-col items-center px-6 md:px-16 mb-6 relative">
       {/* Header */}
-      <h1 className="text-3xl md:text-5xl text-white pb-4 border-b border-white w-full text-center mt-4 font-semibold ">
+      <h1 className="font-bunken text-3xl md:text-5xl text-white pb-4 border-b border-white w-full text-center mt-4 font-semibold">
         TOURNAMENT
       </h1>
+
       {/* PUBG */}
-      <div className="md:my-16 my-8 md:w-[19rem] md:h-[19rem] h-[10rem] w-[10rem] flex items-center justify-center rounded-3xl  ">
+      <div className="md:my-16 my-8 md:w-[19rem] md:h-[19rem] h-[14rem] w-[14rem] flex items-center justify-center rounded-3xl">
         <Image
           src="/tournament-img/BGMI.png"
           alt="BGMI"
@@ -61,15 +62,17 @@ const Tournament = () => {
           className="w-full h-full"
         />
       </div>
+
       {/* Other Sports */}
-      <div className="grid grid-cols-3 md:gap-[5rem]  gap-[1rem] px-8 items-center">
+      <div className="flex flex-col md:grid md:grid-cols-3 md:gap-[5rem] gap-[1rem] px-8 items-center relative">
         {data.map((item, index) => (
           <div
             key={index}
-            className={`md:w-[14rem] md:h-[14rem]  flex items-center justify-center rounded-3xl ${
+            className={`relative w-[10rem] h-[10rem] md:w-[14rem] md:h-[14rem] flex items-center justify-center rounded-3xl ${
               index === 9 ? "col-span-1 col-start-2" : ""
-            } `}
+            }`}
           >
+            {/* Image */}
             <Image
               src={item.image}
               alt={item.name}
@@ -77,6 +80,88 @@ const Tournament = () => {
               height={300}
               className="w-full h-full"
             />
+
+            {/* Connection Lines */}
+            {/* Vertical Line */}
+            {(index < data.length - 1 && index%2 === 0 )&& (
+              <div
+                className="md:hidden absolute w-[2px] bg-white"
+                style={{
+                  top: "50%",
+                  right: "-4rem",
+                  height: "11rem",
+                }}
+              />
+            )}
+            {(index < data.length - 1 && index%2 !== 0 )&& (
+              <div
+                className="md:hidden absolute w-[2px] bg-white"
+                style={{
+                  top: "50%",
+                  left: "-4rem",
+                  height: "11rem",
+                }}
+              />
+            )}
+
+            {/* Horizontal Line */}
+            {index < data.length && (
+              <div
+                className="md:hidden absolute h-[2px] bg-white"
+                style={{
+                  right: "-4rem",
+                  top: "50%",
+                  width: "3rem",
+                  transform: "translateY(-50%)",
+                }}
+              />
+            )}
+              {
+             
+             (index < data.length && index !== 0  && index !== data.length - 1) && (
+               <div
+               className="md:hidden absolute h-[2px] bg-white"
+               style={{
+                 left: "-4rem",
+                 top: "50%",
+                 width: "3rem",
+                 transform: "translateY(-50%)",
+               }}
+             />
+             )
+         }
+            {/* Connector Circle */}
+            {index === 0 || index === data.length - 1 ? (
+              <div
+                className="md:hidden absolute w-[1rem] h-[1rem] bg-white rounded-full"
+                style={{
+                  right: "-1.5rem",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                }}
+              />
+            ):(
+              <>
+              <div
+                className="md:hidden absolute w-[1rem] h-[1rem] bg-white rounded-full"
+                style={{
+                  left: "-1.5rem",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                }}
+              />
+              <div
+                className="md:hidden absolute w-[1rem] h-[1rem] bg-white rounded-full"
+                style={{
+                  right: "-1.5rem",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                }}
+              />
+              </>
+            )
+          
+          }
           </div>
         ))}
       </div>
